3058df1a26a3b30bf28dd58fbce0517c70b9b0f5
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-09-17 22:25:28 +0600

    birth detailse done

--- src/components/ragistration/Ragistration.jsx
+++ src/components/ragistration/Ragistration.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { useFormik } from "formik";
-import { registrationWarning } from "../registrationWarning/RegistrationWarning";
+import { registrationWarning } from "../registrationWarning/registrationWarning";
 
-let initialState = {
-  fullName: "",
-  email: "",
-  password: "",
-};
 const Ragistration = () => {
-  const formik = useFormik({
-    initialValues: initialState,
-    validationSchema: registrationWarning,
-    onSubmit: () => {
-      console.log("submited");
-    },
-  });
-  console.log(formik.errors.email);
+  let initialValues = {
+    fullName: "",
+    email: "",
+    password: "",
+    bDate: new Date().getDate(),
+    bMonth: new Date().getMonth() + 1,
+    bYear: new Date().getFullYear(),
+  };
 
+  const formik = useFormik({
+    initialValues,
+    onSubmit: console.log("submited"),
+    validationSchema: registrationWarning,
+  });
+
+  let storeYear = new Date().getFullYear();
+  let years = Array.from(new Array(105), (val, index) => storeYear - index);
   let { errors, touched } = formik;
 
   return (
@@ -26,74 +29,76 @@ const Ragistration = () => {
         <h2 className="text-4xl font-bold mb-5 text-orange-500">
           Ragistration form
         </h2>
-        <form onSubmit={formik.handleSubmit} action="">
+        <form onSubmit={formik.handleSubmit}>
           <input
             onChange={formik.handleChange}
             value={formik.values.fullName}
             autoComplete="off"
-            onBlur={formik.handleBlur}
             name="fullName"
             type="text"
-            className={`w-full px-5 py-2 outline-none border border-sky-800 rounded-md ${
-              errors.fullName && touched.fullName ? "mb-1" : "mb-3"
-            } text-lg font-sans`}
+            className="w-full px-5 py-2 outline-none border border-sky-800 rounded-md 
+             text-lg font-sans mb-3"
             placeholder="Enter your name"
           />
           {errors.fullName && touched.fullName && (
-            <div className="text-sm  py-2 text-red-500">{errors.fullName}</div>
+            <div className="text-sm  mb-3 text-red-500">{errors.fullName}</div>
           )}
 
           <input
             onChange={formik.handleChange}
             value={formik.values.email}
             autoComplete="off"
-            onBlur={formik.handleBlur}
             name="email"
             type="email"
-            className={`w-full px-5 py-2 outline-none border border-sky-800 rounded-md ${
-              errors.email && touched.email ? "mb-1" : "mb-3"
-            } text-lg font-sans`}
+            className="w-full px-5 py-2 outline-none border border-sky-800 rounded-md 
+             text-lg font-sans mb-3"
             placeholder="Enter your email"
           />
-          {errors.email && (
-            <div className="text-sm  py-2 text-red-500">{errors.email}</div>
+          {errors.email && touched.email && (
+            <div className="text-sm  mb-3 text-red-500">{errors.email}</div>
           )}
           <input
             onChange={formik.handleChange}
             value={formik.values.password}
             autoComplete="off"
-            onBlur={formik.handleBlur}
             name="password"
             type="password"
-            className={`w-full px-5 py-2 outline-none border border-sky-800 rounded-md ${
-              errors.password && touched.password ? "mb-1" : "mb-3"
-            } text-lg font-sans`}
+            className="w-full px-5 py-2 outline-none border border-sky-800 rounded-md 
+             text-lg font-sans mb-3"
             placeholder="Enter your password"
           />
-          {errors.password && (
-            <div className="text-sm  py-2 text-red-500">{errors.password}</div>
+          {errors.password && touched.password && (
+            <div className="text-sm  mb-3 text-red-500">{errors.password}</div>
           )}
 
           <div className="">
             <h3 className="text-xl">Select your birth date</h3>
             <div className="flex justify-between items-center my-3">
               <select
-                name="date"
+                onChange={formik.handleChange}
+                value={formik.values.bDate}
+                name="bDate"
                 className="border border-yellow-500 rounded-md  w-[30%] outline-none p-2 appearence-none"
               >
                 <option value="">8</option>
               </select>
               <select
-                name="month"
+                onChange={formik.handleChange}
+                value={formik.values.bMonth}
+                name="bMonth"
                 className="border border-yellow-500 rounded-md  w-[30%] outline-none p-2 appearence-none"
               >
                 <option value="">8</option>
               </select>
               <select
-                name="year"
+                onChange={formik.handleChange}
+                value={formik.values.bYear}
+                name="bYear"
                 className="border border-yellow-500 rounded-md  w-[30%] outline-none p-2 appearence-none"
               >
-                <option value="">8</option>
+                {years.map((item, index) => (
+                  <option key={index}>{item}</option>
+                ))}
               </select>
             </div>
           </div>
