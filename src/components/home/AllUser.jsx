5f3fba3e83c012271ad995e5eadd0157aa94af80
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-09-29 14:12:06 +0600

    Try to problem soleved

--- src/components/home/AllUser.jsx
+++ src/components/home/AllUser.jsx
@@ -1,16 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { AddFriendIcon } from "../../assets/svg/AddFriendIcon";
 import Title2 from "../utilities/Title2";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getStorage, ref as Ref } from "firebase/storage";
 import { useSelector } from "react-redux";
-import { getDownloadURL, getStorage, ref as Ref } from "firebase/storage";
+import { getDatabase, ref, onValue } from "firebase/database";
+
 const AllUser = () => {
   let [user, setUser] = useState([]);
 
   const userUid = useSelector((state) => state.login.user);
   const storage = getStorage();
-
   const db = getDatabase();
+
+  // useEffect(() => {
+  //   const starCountRef = ref(db, "users/");
+  //   onValue(starCountRef, (snapshot) => {
+  //     snapshot.forEach((item) => {
+  //       console.log(item.val());
+  //     });
+  //   });
+  // }, []);
   useEffect(() => {
     const userData = ref(db, "users/");
     let users = [];
@@ -42,25 +51,20 @@ const AllUser = () => {
   return (
     <div className="mt-7 px-7">
       <Title2 content="All users" />
-      {user?.map((item, index) => (
-        <div key={index} className="flex justify-between items-center my-4">
-          <div className="flex gap-5 items-center">
-            <div className="w-12 h-12 rounded-full overflow-hidden">
-              <img
-                src={item.photoURL}
-                alt="profile Photo"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="">
-              <h3>{item.username}</h3>
-            </div>
+
+      <div className="flex justify-between items-center my-4">
+        <div className="flex gap-5 items-center">
+          <div className="w-12 h-12 rounded-full overflow-hidden">
+            <img />
           </div>
           <div className="">
-            <AddFriendIcon />
+            <h3>dfgh</h3>
           </div>
         </div>
-      ))}
+        <div className="">
+          <AddFriendIcon />
+        </div>
+      </div>
     </div>
   );
 };
