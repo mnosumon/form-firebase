0a1c01610dd4245792be7ca9b27e65a1de2299d5
mnosumon <mdnuruddin378350@gmail.com>
2024-10-18 10:36:12 -0700

    hello

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <div className="flex justify-between items-center w-2/3 mx-auto p-8 shadow-md rounded-md">
       <div className="w-[48%]">
-        <Lottie animationData={LoginAnimation} loop={true} />;
+        {/* <Lottie animationData={LoginAnimation} loop={true} />; */}
       </div>
       <div className="w-[48%]">
         <LoginForm />
