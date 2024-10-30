baa27c1de65850075e92d033a80a1e1af3492472
mnosumon <mdnuruddin378350@gmail.com>
2024-10-30 20:00:49 +0600

    Send request data write done

--- src/components/home/AllUser.jsx
+++ src/components/home/AllUser.jsx
@@ -3,13 +3,14 @@ import { AddFriendIcon } from "../../assets/svg/AddFriendIcon";
 import Title2 from "../utilities/Title2";
 import { getDownloadURL, getStorage, ref as Ref } from "firebase/storage";
 import { useSelector } from "react-redux";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import AvaterImg from "../../assets/image/avarar.jpg";
+
 const AllUser = () => {
   let [users, setUsers] = useState([]);
-  console.log(users);
 
-  const userUid = useSelector((state) => state.login.user);
+  const user = useSelector((state) => state.login.user);
+
   const storage = getStorage();
   const db = getDatabase();
 
@@ -18,7 +19,7 @@ const AllUser = () => {
     onValue(starCountRef, (snapshot) => {
       let user = [];
       snapshot.forEach((item) => {
-        if (item.key !== userUid.uid) {
+        if (item.key !== user.uid) {
           getDownloadURL(Ref(storage, item.key))
             .then((downloadURL) => {
               user.push({
@@ -40,13 +41,26 @@ const AllUser = () => {
         }
       });
     });
-  }, [db, userUid.uid, storage]);
+  }, [db, user.uid, storage]);
+
+  const handleReqSend = (item) => {
+    console.log(item);
+
+    set(push(ref(db, "friendReqUserDetails/")), {
+      senderName: user.displayName,
+      senderID: user.uid,
+      senderPhoto: user.photoURL ?? AvaterImg,
+      recieverName: item.username,
+      recieverID: item.id,
+      recieverPhoto: item.photoURL ?? AvaterImg,
+    });
+  };
 
   return (
     <div className="mt-7 px-7">
       <Title2 content="All users" />
       {users?.map((item, index) => (
-        <div className="flex justify-between items-center my-4">
+        <div key={index} className="flex justify-between items-center my-4">
           <div className="flex gap-5 items-center">
             <div className="w-12 h-12 rounded-full overflow-hidden">
               <img src={item.photoURL || AvaterImg} />
@@ -55,7 +69,7 @@ const AllUser = () => {
               <h3>{item.username}</h3>
             </div>
           </div>
-          <div className="">
+          <div onClick={() => handleReqSend(item)} className="cursor-pointer">
             <AddFriendIcon />
           </div>
         </div>
