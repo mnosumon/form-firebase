ae487f6bf0d6132bd17b03c62bc993b00b463633
mnosumon <mdnuruddin378350@gmail.com>
2024-11-04 21:50:48 +0600

    send message data write done

--- src/components/message/SendMessage.jsx
+++ src/components/message/SendMessage.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Imoji } from "../../assets/svg/Imoji";
 import { GallaryIcon } from "../../assets/svg/GallaryIcon";
 import { useSelector } from "react-redux";
 import AvaterImg from "../../assets/image/avarar.jpg";
+import { getDatabase, push, ref, set } from "firebase/database";
+import { date } from "yup";
 
 const SendMessage = () => {
-  const user = useSelector((state) => state.single.value);
+  const singleFriend = useSelector((state) => state.single.value);
+  const user = useSelector((state) => state.login.user);
+  const [text, setText] = useState("");
+  const timeFunction = `${new Date().getFullYear()}-${
+    new Date().getMonth() + 1
+  }-${new Date().getDate()}  ${new Date().getHours()}:${new Date().getMinutes()}`;
+
+  const db = getDatabase();
+
+  const handleMessage = () => {
+    if (singleFriend?.status === "single") {
+      set(push(ref(db, "singleMessage/")), {
+        whoSenderId: user.uid,
+        whoSenderName: user.displayName,
+        whoRecieverId: singleFriend.id,
+        whoRecieverName: singleFriend.name,
+        text: text,
+        time: timeFunction,
+      });
+    }
+    setText("");
+  };
   return (
     <div className="pt-2 pr-5">
       <div className="bg-[#232323] flex items-center gap-x-5 px-10 py-3 rounded-t-md">
         <div className="w-12 h-12 rounded-full overflow-hidden">
-          <img src={user.photo || AvaterImg} />
+          <img src={singleFriend.photo || AvaterImg} />
         </div>
         <div className="">
-          <h3 className="text-white">{user.name}</h3>
+          <h3 className="text-white">{singleFriend.name}</h3>
         </div>
       </div>
       <div className="w-full h-[500px] shadow-md bg-red-200 px-5 py-1 overflow-y-auto">
@@ -59,13 +82,18 @@ const SendMessage = () => {
           </div>
           <div className="w-3/4">
             <input
+              value={text}
+              onChange={(e) => setText(e.target.value)}
               className="w-full py-3 px-4 outline-none"
               placeholder="type something"
               type="text"
             />
           </div>
           <div className="w-[10%]">
-            <button className="text-sm font-mono font-normal py-3 px-8 bg-[#4A81D3] text-white rounded-md">
+            <button
+              onClick={handleMessage}
+              className="text-sm font-mono font-normal py-3 px-8 bg-[#4A81D3] text-white rounded-md"
+            >
               Send
             </button>
           </div>
